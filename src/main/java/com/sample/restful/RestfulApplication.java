9763b7bd8743b4af7ea5c4602fee9732a116d37e
sanche <sanche@localhost>
2020-12-09 14:01:45 +0900

    test

--- src/main/java/com/sample/restful/RestfulApplication.java
+++ src/main/java/com/sample/restful/RestfulApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulApplication {
 
+	/**
+	 * 
+	 * @param args hi ~~ 
+	 */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApplication.class, args);
 	}
